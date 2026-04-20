d2f254f90b72c4e80ed57a5cc5d64a8dc068aa81
w10187267 <126286697+jorfeen@users.noreply.github.com>
2026-04-20 15:46:09 -0500

    Restore default backend port to 5000

--- backend/run_simple.py
+++ backend/run_simple.py
@@ -8,5 +8,5 @@ import os
 
 if __name__ == '__main__':
     app = create_app()
-    port = int(os.environ.get('PORT', 5001))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
