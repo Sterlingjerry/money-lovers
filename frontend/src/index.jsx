5d45d361ce22cb9a0728b42ac53e1da50ed8e7c1
Jmejia <84045999+Josemejia1@users.noreply.github.com>
2026-04-23 12:34:23 -0500

    Change CSS import from App.css to styles.css

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,7 +5,7 @@
 
 import React, { useState, useEffect } from 'react';
 import api from './api';
-import './App.css';
+import './styles.css';
 
 function App() {
   const [page, setPage] = useState('login');
